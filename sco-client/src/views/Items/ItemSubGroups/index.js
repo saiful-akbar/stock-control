2143c6fe9c2ad2745cca25fb41706b0ea0112c99
saiful-akbar <saifulakbar.app@gmail.com>
2021-04-22 21:41:29 +0700

    Update komponen ItemSubGroups

--- sco-client/src/views/Items/ItemSubGroups/index.js
+++ sco-client/src/views/Items/ItemSubGroups/index.js
@@ -1,46 +1,48 @@
 import React from 'react';
 import ItemSubGroupTable from './ItemSubGroupsTable';
 import ItemSubGroupsForm from './ItemSubGroupsForm';
-
+import { useLocation } from 'react-router';
 
 // Komponen utama
 function ItemSubGroup(props) {
   const [isOpenForm, setOpenForm] = React.useState({
-    open: true,
+    open: false,
     type: null,
     data: null
   });
 
+  const query = new URLSearchParams(useLocation().search);
+
+  React.useEffect(() => {
+    console.log('Location: ', query.get('tab'));
+  }, [query]);
 
   return (
     <>
-      {isOpenForm.open
-        ? (
-          <ItemSubGroupsForm
-            type={isOpenForm.type}
-            onClose={() => {
-              setOpenForm({
-                open: false,
-                type: null,
-                data: null
-              });
-            }}
-          />
-        ) : (
-          <ItemSubGroupTable
-            onAdd={() => {
-              setOpenForm({
-                open: true,
-                type: "Add",
-                data: null,
-              });
-            }}
-          />
-        )
-      }
+      {isOpenForm.open ? (
+        <ItemSubGroupsForm
+          type={isOpenForm.type}
+          onClose={() => {
+            setOpenForm({
+              open: false,
+              type: null,
+              data: null
+            });
+          }}
+        />
+      ) : (
+        <ItemSubGroupTable
+          onAdd={() => {
+            setOpenForm({
+              open: true,
+              type: 'Add',
+              data: null
+            });
+          }}
+        />
+      )}
     </>
-  )
+  );
 }
-
 
 export default ItemSubGroup;
